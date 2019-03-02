695c523868e9cc46a57a54669809c958106cb9ac
thegouge <thegouge42@gmail.com>
2019-03-01 18:18:24 -0800

    updated some of the logic & configured eslint

---
+++ index.js
@@ -0,0 +1,53 @@
+var buttons = [
+  document.getElementById("1"),
+  document.getElementById("2"),
+  document.getElementById("3"),
+  document.getElementById("4"),
+  document.getElementById("5"),
+  document.getElementById("6"),
+  document.getElementById("7"),
+  document.getElementById("8"),
+  document.getElementById("9"),
+  document.getElementById("0"),
+  document.getElementById("c"),
+  document.getElementById("h"),
+  document.getElementById("m"),
+  document.getElementById("s"),
+  document.getElementById("x"),
+  document.getElementById("d"),
+  document.getElementById("pl"),
+  document.getElementById("min"),
+  document.getElementById("del"),
+  document.getElementById("equals")
+];
+
+var output = document.getElementById("output");
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    input(button.id);
+  });
+});
+
+function calculate(input) {
+  var result = "";
+
+  try {
+    input.match(/\d/)[0];
+  } catch (error) {
+    alert("Invalid input!");
+  }
+
+  input = input.split(" ");
+  input.forEach((string) => {
+    if (string.length > 1) {
+      string = toSeconds(string);
+    }
+
+    result = result + string;
+  });
+
+  result = eval(result);
+
+  notate(result);
+}
---
+++ logic.js
@@ -0,0 +1,50 @@
+function toSeconds(inputString) {
+  const letterMatch = inputString.match(/\d{1,}[hms]/g);
+
+  return letterMatch
+    .map((timePartial) => {
+      const partialEnd = timePartial.length - 1;
+      const number = parseInt(timePartial.substring(0, partialEnd));
+      switch (timePartial[partialEnd]) {
+        case "h":
+          return number * 3600;
+
+        case "m":
+          return number * 60;
+
+        case "s":
+          return number;
+
+        default:
+          return 0;
+      }
+    })
+    .reduce((acc, curr) => acc + curr, 0);
+}
+
+function toMinutes(seconds) {
+  return `${seconds / 60}m`;
+}
+
+function toHours(seconds) {
+  return `${seconds / 3600}h`;
+}
+
+function notate(inputSeconds) {
+  let seconds = inputSeconds;
+  let hours = 0;
+  let minutes = 0;
+
+  while (seconds >= 60) {
+    seconds -= 60;
+    minutes++;
+  }
+
+  while (minutes >= 60) {
+    minutes -= 60;
+    hours++;
+  }
+
+  var result = `${hours}h${minutes}m${seconds}s`;
+  return result;
+}
