6dad0e1377bc0047d65f70cd4afcf68f4c4de0a3
thegouge <thegouge42@gmail.com>
2019-03-05 22:41:18 -0800

    converted to typescript and made it functional

---
+++ index.ts
@@ -0,0 +1,25 @@
+import "logic";
+import math from "math.js";
+
+exports.evaluateTimeExpression = (inputExpression: string) => {
+  if (!/\d[hms]/.test(inputExpression)) {
+    console.log(
+      "invalid string input for 'evaluateTimeExpression', expecting '*h*m*s' format"
+    );
+    return;
+  }
+
+  const formattedTimes = inputExpression.replace(
+    /([hms])([\/*+-])(\d)/,
+    "$1 $2 $3"
+  );
+
+  const expressionInSeconds = formattedTimes
+    .split(" ")
+    .map((time: string) => {
+      return /[\/*+-]/.test(time) ? time : convertToSeconds(time);
+    })
+    .join(" ");
+
+  return notate(math.eval(expressionInSeconds));
+};
---
+++ logic.ts
@@ -0,0 +1,60 @@
+function formatInput(input: string | number) {
+  if (typeof input === "string") {
+  } else if (typeof input === "number") {
+  }
+  return input;
+}
+
+function convertToSeconds(inputString: string) {
+  const letterMatch = inputString.match(/\d{1,}[hms]/g);
+
+  if (letterMatch) {
+    return letterMatch
+      .map((timePartial) => {
+        const partialEnd = timePartial.length - 1;
+        const number = parseInt(timePartial.substring(0, partialEnd));
+        switch (timePartial[partialEnd]) {
+          case "h":
+            return number * 3600;
+
+          case "m":
+            return number * 60;
+
+          case "s":
+            return number;
+
+          default:
+            return 0;
+        }
+      })
+      .reduce((acc, curr) => acc + curr, 0);
+  } else {
+    return parseInt(inputString);
+  }
+}
+
+function convertToMinutes(seconds: number) {
+  return `${seconds / 60}m`;
+}
+
+function convertToHours(seconds: number) {
+  return `${seconds / 3600}h`;
+}
+
+function notate(inputSeconds: number) {
+  let seconds = inputSeconds;
+  let hours = 0;
+  let minutes = 0;
+
+  while (seconds >= 60) {
+    seconds -= 60;
+    minutes++;
+  }
+
+  while (minutes >= 60) {
+    minutes -= 60;
+    hours++;
+  }
+
+  return `${hours}h${minutes}m${seconds}s`;
+}
